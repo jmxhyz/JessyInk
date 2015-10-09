0a7b151d78a9d083b9c63e672c8c65babfa3935c
ljj <jmxhyz@qq.com>
2015-10-09 23:57:25 +0800

    添加nodePPT演示

--- nodejessy/server.js
+++ nodejessy/server.js
@@ -100,11 +100,11 @@ app.get('/', function (req, res) {
 //    });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  //console.log('a user connected');
   //socket.id
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    //console.log('user disconnected');
   });
     
   socket.on('chat message', function(msg){
